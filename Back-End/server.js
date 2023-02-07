70795ccb7e4669406b697456bc4d824c307ec4a1
cristi <munteanu.cristian1009@gmail.com>
2023-02-07 17:37:59 +0200

    CRUD Opperations for Parts done

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,28 +2,20 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const routes = require('./Routes');
-const cookieParser = require('cookie-parser');
+const config = require('./Util/config')
+const routes = require('./routes')
 
-// const { config } = require('./firebase/firebase.config');
-const dotenv = require('dotenv');
-
-const PORT = 5001;
 
 const app = express();
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
+app.use(express.json());
 app.use(cors());
-dotenv.config();
+app.use(bodyParser.json());
 
+app.listen(config.port, () =>
+console.log(`App is listening on port:${config.port}`)
+)
 
-app.use("/test", routes);
+app.use('/api', routes);
 
-app.listen(PORT, () => {
-    // console.log(`App is running on http://localhost:${process.env.PORT}`);
-    console.log(`App is running on http://localhost:${PORT}`);
-
-
-});
+// const dotenv = require('dotenv');
